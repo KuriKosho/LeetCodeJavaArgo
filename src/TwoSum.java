ffbb315d0f449bb3331cff45a7ec48b5f3d8edf0
Kuri Kosho <vuongvanthong113.work@gmail.com>
2025-02-10 13:35:21 +0700

    main

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,20 +1,25 @@
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
+
         for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(target - nums[i])) {
-                return new int[]{map.get(target - nums[i]), i};
+            int needed = target - nums[i]; // Số cần tìm
+            if (map.containsKey(needed)) {
+                return new int[]{map.get(needed), i}; // Trả về vị trí của hai số
             }
+            map.put(nums[i], i); // Lưu {giá trị: vị trí}
         }
-        return new int[0];
+
+        return new int[]{};
     }
     public static void main(String[] args) {
         TwoSum ts = new TwoSum();
-        int[] nums = {2, 7, 11, 15};
-        int target = 9;
+        int[] nums = {3,2,4};
+        int target = 6;
         int[] res = ts.twoSum(nums, target);
-        System.out.println(res);
+        System.out.println(Arrays.toString(res));
     }
 }
