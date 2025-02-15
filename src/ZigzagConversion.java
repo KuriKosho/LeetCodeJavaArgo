84686d6f6874c8c0d59315b2ab4853420b4174ad
Kuri Kosho <vuongvanthong113.work@gmail.com>
2025-02-15 16:15:13 +0700

    day 6

---
+++ src/ZigzagConversion.java
@@ -0,0 +1,32 @@
+public class ZigzagConversion {
+        public static String convert(String s, int numRows) {
+            if (numRows == 1) return s;
+
+            int inc = numRows * 2 - 2;
+            int len = s.length();
+            char[] res = new char[len];
+            int idx = 0;
+//            First row
+            for (int j = 0; j < len; j += inc)
+                res[idx++] = s.charAt(j);
+
+//            Middle rows
+            for (int i = 1; i < numRows - 1; i++) {
+                for (int j = i; j < len; j += inc) {
+                    res[idx++] = s.charAt(j);
+                    if (i != 0 && i != numRows - 1 && (j + inc - 2 * i) < len)
+                        res[idx++] = s.charAt(j + inc - 2 * i);
+                }
+            }
+            for (int j = numRows - 1; j < len; j += inc)
+                res[idx++] = s.charAt(j);
+            return new String(res);
+        }
+
+        // Test
+        public static void main(String[] args) {
+            System.out.println(convert("PAYPALISHIRING", 3));  // Output: "PAHNAPLSIIGYIR"
+            System.out.println(convert("PAYPALISHIRING", 4));  // Output: "PINALSIGYAHRPI"
+        }
+
+}
